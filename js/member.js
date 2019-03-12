7dff29f4b9a411a6f1ac8df3fa4d42b87b76780d
musker <aery_mzc9123@163.com>
2019-03-12 14:07:21 +0800

    first commit

---
+++ js/member.js
@@ -0,0 +1,18 @@
+var member = [
+  {
+    "name": "test1",
+    "phone": "xxx"
+  },
+  {
+    "name": "test2",
+    "phone": "xxx"
+  },
+  {
+    "name": "test3",
+    "phone": "xxx"
+  },
+  {
+    "name": "test4",
+    "phone": "xxx"
+  }
+];
